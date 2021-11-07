14262867a20fb97a9fe20ee01702deeffdf1dd79
Priyanshu <priyanshu2000>
2021-11-07 15:49:30 +0530

    added splash screen and assets folder

--- android/app/src/main/java/com/stockapp/MainActivity.java
+++ android/app/src/main/java/com/stockapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.stockapp;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "StockApp";
+  }
+  @Override
+
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
   }
 }
